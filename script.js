f497f747d0e17a93a39d47379777b7451a956fa9
amandaaab <amandaaab@users.noreply.github.com>
2018-01-02 11:01:10 +0100

    In/utloggning

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
 $(document).ready(function(){
 
+    var users;
 
+       //Hämtar users.json och sparar i en variabel (users)
+       fetch("json/kunder.json")
+       .then(function(response) {
+           return response.json();
+       })
+       .then(function(ourUsers) {
+           users = ourUsers;
+       });
+
+       $(".loggaUt").hide();
+   //       INLOGGNING
+       if(sessionStorage.saveUser != null ){
+        thisUserIsLoggedIn();
+        }else{
+    
+            $(".buttonForm").click(function(){
+            for(var i = 0; i < users.length; i++){
+                
+                if( $(".mailForm").val() == users[i].email && $(".passwordForm").val() == users[i].password){
+    
+                    thisUserIsLoggedIn();
+                    sessionStorage.saveUser = users[i].email;
+                    console.log("heeeej");
+                    }else{
+                        console.log("glömt ditt lösenord");
+                    }
+                }
+            })
+        }
+//          Utloggning
+        $(".loggaUt").click(function(){
+            sessionStorage.removeItem("saveUser");
+            location.reload();
+        });
+
+
+
+
+ //         Funktioner
+        function thisUserIsLoggedIn(){
+            $(".loginform").hide();
+            $(".loggaIn").hide();
+            $(".bliMedlem").hide();
+            $(".loggaUt").show();
+        }
+
+      
+        
 
 });

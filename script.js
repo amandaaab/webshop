2a04dd1d57c2c4eed6a23187e83a879bf69873ef
amandaaab <amandaaab@users.noreply.github.com>
2018-01-03 15:18:49 +0100

    underkategori funktioner

--- script.js
+++ script.js
@@ -1,54 +1,139 @@
 $(document).ready(function(){
 
     var users;
+    var huvudkategori;
+    var kategori;
+    var produkt;
+   
 
        //Hämtar users.json och sparar i en variabel (users)
        fetch("json/kunder.json")
        .then(function(response) {
            return response.json();
+           
        })
        .then(function(ourUsers) {
            users = ourUsers;
+           console.log(users);
+           
+           
        });
 
-       $(".loggaUt").hide();
-   //       INLOGGNING
-       if(sessionStorage.saveUser != null ){
-        thisUserIsLoggedIn();
-        }else{
+       fetch("json/huvudkategorier.json")
+       .then(function(response) {
+           return response.json();
+       })
+   
+       .then(function(huvud) {
+           huvudkategori = huvud;
+          
+           
+    });
+
+
+    fetch("json/underkategorier.json")
+    .then(function(response) {
+        return response.json();           
+    })
+     .then(function(under) {
+        kategori = under;
+
+       
+
+    });
+
+    fetch("json/produkter.json")
+    .then(function(response) {
+        return response.json();           
+    })
+     .then(function(item) {
+        produkt = item;
+        console.log(produkt);
+        allScriptCode();
+    });
     
-            $(".buttonForm").click(function(){
-            for(var i = 0; i < users.length; i++){
-                
-                if( $(".mailForm").val() == users[i].email && $(".passwordForm").val() == users[i].password){
-    
-                    thisUserIsLoggedIn();
-                    sessionStorage.saveUser = users[i].email;
-                    console.log("heeeej");
-                    }else{
-                        console.log("glömt ditt lösenord");
-                    }
-                }
-            })
+
+    function allScriptCode(){
+
+        loopHuvudKategori();
+        loopUnderkategori();
+        
+        console.log(huvudkategori);
+        console.log(kategori);
+        console.log(users);
+        console.log("hej:" + produkt);
+
+        //Lopar och skriver ut huvudkategorierna
+        function loopHuvudKategori(){
+        
+            for(var i = 0; i < huvudkategori.length; i++){
+                $(".mainmeny").append("<div class='headitem'><a href='#' onclick='visaVarde("+ huvudkategori[i].id +")'>" + huvudkategori[i].room + "<i class='fa fa-angle-down' aria-hidden='true'></i></a></div>");
+            }
+
         }
-//          Utloggning
-        $(".loggaUt").click(function(){
-            sessionStorage.removeItem("saveUser");
-            location.reload();
-        });
+
+        //Lopar och skriver ut underkategorierna
+        function loopUnderkategori(){
+            visaVarde = function(val){
+                
+                console.log ("hejsan" + val);
+                var visaKategori = val;
+
+                $(".dropdownmenu").html(" ");
+
+                for(i = 0; i < kategori.length; i++) {
+                    if (kategori[i].huvudkategori == visaKategori){
+                        $(".dropdownmenu").append("<li>" + kategori[i].underkategori + "</li>")
+                    }
+
+                    $(".underKategori").show();
+                    $(".dropdownmenu").show();
+
+                }
+
+            }
+
+        }
+
+        $(".loggaUt").hide();
+
+            //       INLOGGNING
+           if(sessionStorage.saveUser != null ){
+               thisUserIsLoggedIn();
+               }else{
+           
+                   $(".buttonForm").click(function(){
+                   for(var i = 0; i < users.length; i++){
+                       
+                       if( $(".mailForm").val() == users[i].email && $(".passwordForm").val() == users[i].password){
+           
+                           thisUserIsLoggedIn();
+                           sessionStorage.saveUser = users[i].email;
+                           console.log("heeeej");
+                           }else{
+                               console.log("glömt ditt lösenord");
+                           }
+                       }
+                   })
+               }
+   
+           //          Utloggning
+            $(".loggaUt").click(function(){
+                   sessionStorage.removeItem("saveUser");
+                   location.reload();
+            
+            });
 
 
-
-
- //         Funktioner
+        //         Funktioner
         function thisUserIsLoggedIn(){
             $(".loginform").hide();
             $(".loggaIn").hide();
             $(".bliMedlem").hide();
             $(".loggaUt").show();
         }
+   
 
-      
-        
+    }
 
 });

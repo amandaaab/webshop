eb91643a84062f22bcdfd9dea84398e265f5a82c
amandaaab <amandaaab@users.noreply.github.com>
2018-01-08 10:39:56 +0100

    varor till kundvagn och ändringar av divs

--- script.js
+++ script.js
@@ -1,203 +1,248 @@
 $(document).ready(function(){
-
-    var users;
-    var huvudkategori;
-    var kategori;
-    var produkt;
-   
-
-       //Hämtar users.json och sparar i en variabel (users)
-       fetch("json/kunder.json")
-       .then(function(response) {
-           return response.json();
-           
-       })
-       .then(function(ourUsers) {
-           users = ourUsers;
-           console.log(users);
-           
-           
-       });
-
-       fetch("json/huvudkategorier.json")
-       .then(function(response) {
-           return response.json();
-       })
-   
-       .then(function(huvud) {
-           huvudkategori = huvud;
-          
-           
-    });
-
-
-    fetch("json/underkategorier.json")
-    .then(function(response) {
-        return response.json();           
-    })
-     .then(function(under) {
-        kategori = under;
-
-       
-
-    });
-
-    fetch("json/produkter.json")
-    .then(function(response) {
-        return response.json();           
-    })
-     .then(function(item) {
-        produkt = item;
-        console.log(produkt);
-        allScriptCode();
-    });
     
+        var users;
+        var huvudkategori;
+        var kategori;
+        var produkt;
+        var shoppingCart = [];
+       
+           //Hämtar users.json och sparar i en variabel (users)
+           fetch("json/kunder.json")
+           .then(function(response) {
+               return response.json();
+               
+           })
+           .then(function(ourUsers) {
+               users = ourUsers;
+               console.log(users);
+               
+           });
+    
+           fetch("json/huvudkategorier.json")
+           .then(function(response) {
+               return response.json();
+           })
+       
+           .then(function(huvud) {
+               huvudkategori = huvud;
+              
+        });
+    
+    
+        fetch("json/underkategorier.json")
+        .then(function(response) {
+            return response.json();           
+        })
+         .then(function(under) {
+            kategori = under;
+    
+        });
+    
+        fetch("json/produkter.json")
+        .then(function(response) {
+            return response.json();           
+        })
+         .then(function(item) {
+            produkt = item;
+            console.log(produkt);
+            allScriptCode();
+            
+        });
+    
+        function allScriptCode(){
+    
+            loopHuvudKategori();
+            loopUnderkategori();
+            loopProdukter();
+            loopSpecProdukt();
+            
+            //                                    FÖRSTASIDAN
+           
 
-    function allScriptCode(){
+            $(".main1").show();
+            $(".homebox1").show();
+            $(".homebox2").show();
 
-        loopHuvudKategori();
-        loopUnderkategori();
-        loopProdukter();
-        loopSpecProdukt();
-        $(".produktWrap").hide();
+            console.log(huvudkategori);
+            console.log(kategori);
+            console.log(users);
+    
+            //                               HUVUDKATEGORIERNA
+            function loopHuvudKategori(){
+            //$(".main1").html(" ");
         
-        console.log(huvudkategori);
-        console.log(kategori);
-        console.log(users);
-        console.log("hej:" + produkt);
 
-        //Lopar och skriver ut huvudkategorierna
-        function loopHuvudKategori(){
-        
-            for(var i = 0; i < huvudkategori.length; i++){
-                $(".mainmeny").append("<div class='headitem'><a href='#' onclick='visaVarde("+ huvudkategori[i].id +")'>" + huvudkategori[i].room + "<i class='fa fa-angle-down' aria-hidden='true'></i></a></div>");
+                for(var i = 0; i < huvudkategori.length; i++){
+                   
+                    $(".mainmeny").append("<div class='headitem'><a href='#' onclick='visaVarde("+ huvudkategori[i].id +")'>" + huvudkategori[i].room + "<i class='fa fa-angle-down' aria-hidden='true'></i></a></div>");
+                    
+
+                   $(".cart").click(function(){
+                    $(".cardInCart").html(" ");
+
+                    addToCart();
+
+                    });
+                    
+                  
+                }
+    
             }
+    
+            //                            UNDERKATEGORIERNA
+            function loopUnderkategori(){
+                visaVarde = function(val){
+                    console.log ("hejsan" + val);
+    
+                    $(".dropdownmenu").html(" ");
+                    $(".main1").html(" ");
+    
+                    for(i = 0; i < kategori.length; i++) {
+                        if (kategori[i].huvudkategori == val){
+                            $(".dropdownmenu").append("<li><a href='#' onclick='visaVardeProdukt("+ kategori[i].id +")'>" + kategori[i].underkategori + "</li>")
+                        }
+                            $(".underKategori").show();
+                            $(".dropdownmenu").show();
+                    }
+                }
+           }
+              
+            //                                PRODUKTERNA
+           function loopProdukter(){
+    
+                visaVardeProdukt = function(val){
+                    console.log ("hej" + val);
+    
+                    $(".main1").html(" ");
+                    $(".produktItem").html(" ");
 
-        }
+    
+                    for(var i = 0; i < produkt.length; i++){
+                        if (produkt[i].underkategori == val){
+                           
+                        $(".main1").append("<div class='produktItem' onclick='visaSpecProdukt("+ produkt[i].id +")'><img src='./img/sovrum/" + produkt[i].image + "'>" + "<div class='prisTitle'>" + "<h3>" +  produkt[i].prodName + "</h3>" + "<h3>" + produkt[i].prodPrice + "kr" + "<h3>" + "</div>" + "</div>");
+                        }
+                    }
+                }
+            }
+    
+            //                             SPECIFIK PRODUKTSIDA
+           function loopSpecProdukt(){
+                visaSpecProdukt = function(val){
+                    console.log(val);
+              $(".main1").html(" ");
+                    var spec = val;
+    
+                   
 
-        //Lopar och skriver ut underkategorierna
-        function loopUnderkategori(){
-            visaVarde = function(val){
+                    for(var i = 0; i < produkt.length; i++){
+                        if(produkt[i].id == spec){
+
+                            var imgProdCard = "<div class='imgProdCard'>" + "<img src='./img/sovrum/" + produkt[i].image + "'>" + "</div>";
+                            var infoH2 = "<h2>" + produkt[i].prodName + "</h2>"; 
+                            var infoP = "<p>" + produkt[i].prodDesc + "</p>";
+                            var infoPrice = "<h3>" + produkt[i].prodPrice + "kr" + "</h3>";
+                            var ProdToCart = "<button class='prodToCart'onClick='pushToCart(" + produkt[i].id + ")'>" + "LÄGG TILL I VARUKORG" + "</div>";
+                            var textProdCard = "<div class='textProdCard'>" + infoH2 + infoP + infoPrice + ProdToCart + "</div>";
+                            
+                            $(".main1").append(imgProdCard + textProdCard);
+
+                            
+                            //console.log("produkt" + produkt);
+                        }
+                    }
+                }
+            }
+    
+            //                            VARUKORG
+    
+            pushToCart = function(val){
+
+                var cart = produkt[val-1];  
+                shoppingCart.push(cart);
+               
+                console.log(cart);
+        
+            }
                 
-                console.log ("hejsan" + val);
-                var visaKategori = val;
+          
+               
+            addToCart = function(){
 
-                $(".dropdownmenu").html(" ");
+                $(".main1").html(" ");
+                $(".main1").append("<h2>Varukorg</h2>");
+            
+                   var json_str = JSON.stringify(shoppingCart);
+                   localStorage.shopping = json_str;
+                   var loopItemCart = JSON.parse(localStorage.shopping);
 
-                for(i = 0; i < kategori.length; i++) {
-                    if (kategori[i].huvudkategori == visaKategori){
-                        $(".dropdownmenu").append("<li><a href='#' onclick='visaVardeProdukt("+ kategori[i].id +")'>" + kategori[i].underkategori + "</li>")
-                    }
-                        $(".underKategori").show();
-                        $(".dropdownmenu").show();
+    
+                for(var i = 0; i < loopItemCart.length; i++){
 
-                }
+                    var imgCartCard = "<div class='imgCartCard'>" + "<img src='./img/sovrum/" + loopItemCart[i].image + "'>" + "</div>";
+                    var cartCardTitle = "<h2 class='cartName'>" + loopItemCart[i].prodName + "</h2>";
+                    var priceCard = "<h2 class='cartName'>" + loopItemCart[i].prodPrice + "kr</h2>";
+                    var cardInCart = "<div class='cardInCart'>" + imgCartCard + cartCardTitle + priceCard + "</div>";
 
-            }
-        }
-
-        function loopProdukter(){
-
-            visaVardeProdukt = function(val){
-                var visaProdukt = val;
-                console.log ("hej" + val);
-
-                $(".produktItem").html(" ");
-
-                for(var i = 0; i < produkt.length; i++){
-                    if (produkt[i].underkategori == visaProdukt){
-                        $(".homebox1").hide();
-                        $(".homebox2").hide();
-                        $(".produktWrap").append("<div class='produktItem' onclick='visaSpecProdukt("+ produkt[i].id +")'><img src='/img/sovrum/" + produkt[i].image + "'>" +  produkt[i].prodName + produkt[i].prodPrice + "</div>");
-                        $(".produktwrap").show();
-                    }
-                    $(".produkterKategori").show();
-                    $(".produkter").show();
-
+                $(".main1").append(cardInCart);
+                console.log(shoppingCart);
+                //console.log(loopItemCart);
+                
                 }
             }
-        }
 
-        function loopSpecProdukt(){
-            visaSpecProdukt = function(val){
-                var visaSpec = val;
-                console.log("spec:" + val);
-
-                //$(".").html()
-                for(var i = 0; i < produkt.length; i++){
-                    if(produkt[i].id == visaSpec){
-                        var imgProdCard = "<div class='imgProdCard'>" + "<img src='/img/sovrum/" + produkt[i].image + "'>" + "</div>";
-                        var infoH2 = "<h2>" + produkt[i].prodName + "</h2>"; 
-                        var infoP = "<p>" + produkt[i].prodDesc + "</p>";
-                        var ProdToCart = "<button class='prodToCart'>" + "Lägg till i varukorg" + "</div>";
-                        var textProdCard = "<div class='textProdcard'>" + infoH2 + infoP + ProdToCart + "</div>";
-
-                        $(".produktItem").hide();
-                        $(".produktWrap").hide();
-                        $(".specWrap").append(imgProdCard + textProdCard);
-                        $(".specWrap").show();
-                    }
-                    $(".produkterKategori").hide();
-                    $(".produkter").hide();
-                }
-            }
-        }
-
-
-
-
-
-        $(".loggaUt").hide();
-        $(".loginForm").hide();
-
-        $(".loggaIn").click(function(){
-
+        
+            //                              LOGGA IN/UT
+    
+    
+            $(".loggaUt").hide();
+            $(".loginform").hide();
+    
+            $(".loggaIn").click(function(){
             $(".homebox1").hide();
             $(".homebox2").hide();
-            $(".loginForm").show();
-
-
-        });
-
-
-            //       INLOGGNING
-           if(sessionStorage.saveUser != null ){
-               thisUserIsLoggedIn();
-               }else{
-           
-                   $(".buttonForm").click(function(){
-                   for(var i = 0; i < users.length; i++){
-                       
-                       if( $(".mailForm").val() == users[i].email && $(".passwordForm").val() == users[i].password){
-           
-                           thisUserIsLoggedIn();
-                           sessionStorage.saveUser = users[i].email;
-                           console.log("heeeej");
-                           }else{
-                               console.log("glömt ditt lösenord");
-                           }
-                       }
-                   })
-               }
-   
-           //          Utloggning
-            $(".loggaUt").click(function(){
-                   sessionStorage.clear();
-                   location.reload();
-                   $(".main1").show();
-            
+              
+            $(".loginform").show();
+    
             });
-
-
-        //         Funktioner
-        function thisUserIsLoggedIn(){
-            $(".loginform").hide();
-            $(".loggaIn").hide();
-            $(".bliMedlem").hide();
-            $(".loggaUt").show();
+    
+                //                                     INLOGGNING
+               if(sessionStorage.saveUser != null ){
+                   thisUserIsLoggedIn();
+                   }else{
+               
+                       $(".buttonForm").click(function(){
+                       for(var i = 0; i < users.length; i++){
+                           
+                           if( $(".mailForm").val() == users[i].email && $(".passwordForm").val() == users[i].password){
+               
+                               thisUserIsLoggedIn();
+                               sessionStorage.saveUser = users[i].email;
+                               console.log("heeeej");
+                               }else{
+                                   console.log("glömt ditt lösenord");
+                               }
+                           }
+                       });
+                   }
+       
+               //                                        Utloggning
+                $(".loggaUt").click(function(){
+                       sessionStorage.clear();
+                       location.reload();
+                    
+                
+                });
+    
+            function thisUserIsLoggedIn(){
+               
+                $(".loginform").hide();
+                $(".loggaIn").hide();
+                $(".bliMedlem").hide();
+                $(".loggaUt").show();
+            }
+       
+    
         }
-   
-
-    }
-
-});
+    
+    });
